f2f2915fe6d1bee99ef5afc9ab5d81781faf3e97
William Marchesi <wmarchesi123@gmail.com>
2014-12-11 16:41:42 -0500

    +Darude Sandstorm Works! ;)

--- DarudeSandstorm.c
+++ DarudeSandstorm.c
@@ -1,6 +1,52 @@
 
 void DarudeSandstorm()
 {
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    20); wait1Msec(  222);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    20); wait1Msec(  222);
+	playTone(  440,    10); wait1Msec(  111);
+	playTone(  440,    10); wait1Msec(  111);
+	playTone(  440,    10); wait1Msec(  111);
+	playTone(  440,    10); wait1Msec(  111);
+	playTone(  440,    10); wait1Msec(  111);
+	playTone(  440,    10); wait1Msec(  111);
+	playTone(  440,    20); wait1Msec(  222);
+	playTone(  392,    10); wait1Msec(  111);
+	playTone(  392,    10); wait1Msec(  111);
+	playTone(  392,    10); wait1Msec(  111);
+	playTone(  392,    10); wait1Msec(  111);
+	playTone(  392,    10); wait1Msec(  111);
+	playTone(  392,    10); wait1Msec(  111);
+	playTone(  392,    20); wait1Msec(  222);
+	playTone(  294,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    20); wait1Msec(  222);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    20); wait1Msec(  222);
+	playTone(  440,    10); wait1Msec(  111);
+	playTone(  440,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    10); wait1Msec(  111);
+	playTone(  330,    20); wait1Msec(  222);
 
 
 return;
@@ -8,5 +54,5 @@ return;
 
 task main()
 {
-DarudeSandstorm()
+DarudeSandstorm();
 }
