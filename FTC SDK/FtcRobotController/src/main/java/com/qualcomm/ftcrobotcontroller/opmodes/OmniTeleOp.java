25308889f38a8f369490da6ee5a4fb061ba5684b
William Marchesi <wmarchesi123@gmail.com>
2015-10-16 10:22:36 -0400

    Added Joystick Support... And More!
    +Only reads values from the joystick, doesn't push to the robot.
    +Added scaling to the joystick and motor values
    +Added spot for Telemetry data

--- FTC SDK/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OmniTeleOp.java
+++ FTC SDK/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OmniTeleOp.java
@@ -26,17 +26,54 @@ public class OmniTeleOp extends OpMode{
         motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
         motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
 
+        telemetry.addData("Text", "*** Robot Data***");
+
     }
 
     @Override
     public void loop() {
 
-        
+        float driverLeftStick = -gamepad1.left_stick_y;
+        float driverRightStick = gamepad1.right_stick_x;
 
+        driverLeftStick = Range.clip(driverLeftStick, -1, 1);
+        driverRightStick = Range.clip(driverRightStick, -1, 1);
+
+        driverLeftStick = (float)scaleInput(driverLeftStick);
+        driverRightStick =  (float)scaleInput(driverRightStick);
     }
 
     @Override
     public void stop() {
 
+    }
+
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
     }
 }
