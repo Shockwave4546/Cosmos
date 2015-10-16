bd5bf2351e71517f344d77997c09cc0e5db967f3
William Marchesi <wmarchesi123@gmail.com>
2015-10-16 10:09:49 -0400

    Added the drive motors
    +Configured the drive motors in init()

--- FTC SDK/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OmniTeleOp.java
+++ FTC SDK/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OmniTeleOp.java
@@ -1,7 +1,42 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
 /**
  * Created by Will Marchesi on 10/16/2015.
  */
-public class OmniTeleOp {
+public class OmniTeleOp extends OpMode{
+
+    DcMotor motorFrontLeft;
+    DcMotor motorFrontRight;
+    DcMotor motorBackLeft;
+    DcMotor motorBackRight;
+
+    public OmniTeleOp() {
+
+    }
+
+    @Override
+    public void init() {
+
+        motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+
+    }
+
+    @Override
+    public void loop() {
+
+        
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
 }
