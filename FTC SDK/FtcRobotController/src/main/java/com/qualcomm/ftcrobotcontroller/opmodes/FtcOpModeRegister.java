1862eeb0760db093d64066a14e86300a47757a43
William Marchesi <wmarchesi123@gmail.com>
2015-10-22 09:40:47 -0400

    Created Tank Op Mode
    +Created Tank Op Mode
    +Got rid of sample Op Modes from the Register
    +Added Tank Op Mode to Ftc Register

--- FTC SDK/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FTC SDK/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -57,11 +57,7 @@ public class FtcOpModeRegister implements OpModeRegister {
 
     manager.register("NullOp", NullOp.class);
     manager.register("OmniTeleOp", OmniTeleOp.class);
-
-    manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-    manager.register("K9TeleOp", K9TeleOp.class);
-    manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
+    manager.register("TankOpMode", OmniTeleOp.class);
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
---
+++ FTC SDK/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankOpMode.java
@@ -0,0 +1,7 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+/**
+ * Created by DoverShockwave on 10/22/2015.
+ */
+public class TankOpMode {
+}
