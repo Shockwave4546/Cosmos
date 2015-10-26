c188650f0350496ace495aa5c0abbd596877e677
William Marchesi <wmarchesi123@gmail.com>
2015-10-26 10:10:43 -0400

    Fixed Telemetry Data
    +The robot will now accept input from the joysticks and push it to the
    phones as telemetry data

--- FTC SDK/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankOpMode.java
+++ FTC SDK/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankOpMode.java
@@ -26,20 +26,30 @@ public class TankOpMode extends OpMode{
         motorBackLeft = hardwareMap.dcMotor.get("motorFrontLeft");
         motorBackRight = hardwareMap.dcMotor.get("motorFrontLeft");
 
-        telemetry.addData("Text", "*** Robot Data ***");
-
     }
 
     @Override
     public void loop() {
-        float driverLeftStick = -gamepad1.left_stick_y;
-        float driverRightStick = gamepad1.right_stick_x;
+        float leftY = -gamepad1.left_stick_y;
+        float leftX = gamepad1.left_stick_x;
+        float rightY = -gamepad1.right_stick_y;
+        float rightX = gamepad1.right_stick_x;
 
-        driverLeftStick = Range.clip(driverLeftStick, -1, 1);
-        driverRightStick = Range.clip(driverRightStick, -1, 1);
+        leftY = Range.clip(leftY, -1, 1);
+        leftX = Range.clip(leftX, -1, 1);
+        rightY = Range.clip(rightY, -1, 1);
+        rightX = Range.clip(rightX, -1, 1);
 
-        driverLeftStick = (float)scaleInput(driverLeftStick);
-        driverRightStick =  (float)scaleInput(driverRightStick);
+        leftY = (float)scaleInput(leftY);
+        leftX = (float)scaleInput(leftX);
+        rightY =  (float)scaleInput(rightY);
+        rightX =  (float)scaleInput(rightX);
+
+        telemetry.addData("Text", "*** Robot Data ***");
+        telemetry.addData("leftX", "LeftX" + leftX);
+        telemetry.addData("leftY", "LeftY" + leftY);
+        telemetry.addData("rightX", "RightX" + rightX);
+        telemetry.addData("rightY", "RightY" + rightY);
     }
 
     @Override
